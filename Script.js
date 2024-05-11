5c78230117901a2dbf6d2a06e5733536cc2b17bf
Solomon <solomonkasparov2001@gmail.com>
2024-05-11 22:21:23 +0200

    Guardado definitivo

---
+++ Script.js
@@ -0,0 +1,5 @@
+const pantallaValorAnterior = document.getElementById("valor-anterior");
+const pantallaValorActual = document.getElementById("valor-actual");
+const botonesDigitos = document.querySelectorAll(".digitos");
+const botonesOperadores = document.querySelectorAll(".operador");
+
